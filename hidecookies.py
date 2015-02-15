98212dfd4949aab2c3da3540c8c318d59852c394
Andreas <hello@inderesting.com>
2015-02-15 02:53:17 +0200

    'pytests'

---
+++ hidecookies.py
@@ -0,0 +1,16 @@
+class A(object):
+ 
+    def __init__(self):
+      self.public = 10
+      self.__private = 20
+ 
+    def get_private(self):
+        return self.__private
+ 
+# outside:
+ 
+a = A() # instance of A
+ 
+print(a.public) # OK, 30
+print(a.get_private()) # OK, 20
+# print(a.__private) # fail, available only within A description
